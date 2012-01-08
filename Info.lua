7216e3c7ab903439ae1a719aa7dd6a5b2133c71e
kyl191 <github@kyl191.net>
2012-01-08 23:00:18 +0800

    Revert the dev naming changes
    Conflicts:
    	Info.lua

--- Info.lua
+++ Info.lua
@@ -8,12 +8,12 @@ return {
 	LrSdkVersion = 3.2,
 	LrSdkMinimumVersion = 3.2, -- minimum SDK version required by this plug-in
 
-	LrToolkitIdentifier = 'net.kyl191.lightroom.export.stash.dev',
-	LrPluginName 		= 'Sta.sh Dev',
 	URLHandler			= "UrlHandler.lua",
 	LrPluginInfoUrl     = "http://kyl191.net/code-and-stuff/lightroom-export-to-sta-sh/",
+	LrToolkitIdentifier = 'net.kyl191.lightroom.export.stash',
+	LrPluginName = 'Sta.sh',
 	LrExportServiceProvider = {
-		title = 'Sta.sh Dev',
+		title = 'Sta.sh',
 		file = 'StashExportServiceProvider.lua',
 	},
     LrPluginInfoProvider = "PluginInfoProvider.lua",
