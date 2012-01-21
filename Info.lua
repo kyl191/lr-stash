5e78ea3c1f88514b67b3a1b9fe27be55a210f5d2
kyl191 <github@kyl191.net>
2012-01-21 18:08:38 +0800

    Clean up the Info.lua file

--- Info.lua
+++ Info.lua
@@ -9,8 +9,9 @@ return {
 	LrSdkMinimumVersion = 3.2, -- minimum SDK version required by this plug-in
 
 	LrToolkitIdentifier = 'net.kyl191.lightroom.export.stash.dev',
-	LrPluginName = 'Sta.sh Dev',
-
+	LrPluginName 		= 'Sta.sh Dev',
+	UrlHandler			= "UrlHandler.lua",
+	LrPluginInfoUrl     = "http://kyl191.net/code-and-stuff/lightroom-export-to-sta-sh/"
 	LrExportServiceProvider = {
 		title = 'Sta.sh Dev',
 		file = 'StashExportServiceProvider.lua',
