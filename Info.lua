b593c51a517d399eb6dd8ddca483d09e3059de3d
kyl191 <github@kyl191.net>
2012-01-21 18:21:23 +0800

    Add a missing comma

--- Info.lua
+++ Info.lua
@@ -11,7 +11,7 @@ return {
 	LrToolkitIdentifier = 'net.kyl191.lightroom.export.stash.dev',
 	LrPluginName 		= 'Sta.sh Dev',
 	UrlHandler			= "UrlHandler.lua",
-	LrPluginInfoUrl     = "http://kyl191.net/code-and-stuff/lightroom-export-to-sta-sh/"
+	LrPluginInfoUrl     = "http://kyl191.net/code-and-stuff/lightroom-export-to-sta-sh/",
 	LrExportServiceProvider = {
 		title = 'Sta.sh Dev',
 		file = 'StashExportServiceProvider.lua',
