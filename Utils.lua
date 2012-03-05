ef5292470f5bb0e25c65832f5ae90f95a5dd41f6
Kyle Lexmond <github@kyl191.net>
2012-03-05 10:29:13 +0800

    Add the md5Files function, currently hardcoded to work on the plugin directory. Also, actually return a Lua table so it works.

--- Utils.lua
+++ Utils.lua
@@ -4,9 +4,12 @@ Utils.lua
 Common code for Lightroom plugins
 
 ------------------------------------------------------------------------------]]
-
+local LrMD5 = import 'LrMD5'
+local LrFileUtils = import 'LrFileUtils'
 local logger = import 'LrLogger'( 'Stash' )
 logger:enable("logfile")
+
+Utils = {}
 
 function Utils.logTable(table)
     for k,v in pairs(table) do
@@ -19,3 +22,23 @@ function Utils.logTable(table)
 end
 
 --------------------------------------------------------------------------------
+
+function Utils.md5Files()
+
+    logger:info("Running md5files for directory " .. _PLUGIN.path)
+    local digests = {}
+    for filePath in LrFileUtils.recursiveFiles( _PLUGIN.path ) do
+        local file = assert(io.open(filePath, "rb"))
+        local data = file:read("*all")
+
+        local md5sum = LrMD5.digest(data)
+        logger:info("MD5sum of " .. filePath .. ": " .. md5sum)
+        digests[#digests+1] = {filePath = filePath, md5 = md5sum}
+    end
+
+    Utils.logTable(digests)
+
+
+end
+
+return Utils
