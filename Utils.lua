ef34b51d9290be786b7fd22530e2a3fce0794432
Kyle Lexmond <github@kyl191.net>
2012-03-06 19:04:17 +0800

    Split the post and JSON parsing routines into their own functions.

--- Utils.lua
+++ Utils.lua
@@ -50,12 +50,35 @@ end
 
 function Utils.getJSON( postUrl )
 
+    json = Utils.post( postUrl )
+
+    if json == nil
+        logger:info(postUrl .. " was supposed to return JSON, but didn't.")
+        LrErrors.throwUserError("Server problem! \n Response from server was empty, but we were expecting a JSON response!")
+
+    -- Now that we have valid JSON, decode it, and try to get the status of our request
+    -- If the status is error, show the error to the user, and die.
+    local decode = JSON:decode(json)
+    if decode.status and decode.status == "error" then
+        logger:info("JSON error from Sta.sh")
+        logger:info(json)
+        LrErrors.throwUserError("Error with a JSON response! \n" .. decode.error .. "\n" ..decode.error_description)
+    end
+    Utils.logTable(decode)
+    return decode
+
+
+end
+
+--------------------------------------------------------------------------------
+
+function Utils.post( postUrl )
     -- Do the request
-    local json, headers = LrHttp.post(postUrl, "")
+    local data, headers = LrHttp.post(postUrl, "")
+    logger:info("Called Utils.post for " .. postUrl)
 
     -- If we didn't get a result back, that means there was a transport error
     -- So show that error to the user
-    logger:info("Called Utils.getJSON for " .. postUrl)
 
     if headers and headers.error then
         logger:info("Lightroom network error:")
@@ -63,25 +86,23 @@ function Utils.getJSON( postUrl )
         LrErrors.throwUserError( "Network error: " .. hdrs.error.nativeCode )
     end
 
+    -- Alternatively, the server could throw back an error.
+    -- Only return data if we're sure
     if headers and tonumber(headers.status) > 299 then
         logger:info("Server error:")
         Utils.logTable(headers)
-        logger:info(json)
-        LrErrors.throwUserError( "Error connecting to Sta.sh: Server returned error code " .. headers.status)
+        logger:info(data)
+        LrErrors.throwUserError( "Remote server returned error code " .. headers.status)
     else
-
-        -- Now that we have valid JSON, decode it, and try to get the status of our request
-        -- If the status is error, show the error to the user, and die.
-        local decode = JSON:decode(json)
-        if decode.status and decode.status == "error" then
-            logger:info("JSON error from Sta.sh")
-            logger:info(json)
-            LrErrors.throwUserError("Error with a JSON response! \n" .. decode.error .. "\n" ..decode.error_description)
-        end
-        Utils.logTable(decode)
-        return decode
+        if data ~= nil
+            return data
+        else
+            logger:info("Response for " .. postUrl .. " was empty.")
+            return nil
     end
 
-end
+    return nil
 
+end
+--------------------------------------------------------------------------------
 return Utils
