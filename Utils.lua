29d9ff42758751d60bc5b25fc362bdfca392ff0f
Kyle Lexmond <github@kyl191.net>
2012-03-07 19:54:38 +0800

    Suppress JSON parsing errors in Utils

--- Utils.lua
+++ Utils.lua
@@ -10,13 +10,22 @@ local logger = import 'LrLogger'( 'Stash' )
 logger:enable("logfile")
 
 local LrPathUtils = import 'LrPathUtils'
-JSON = (loadfile (LrPathUtils.child(_PLUGIN.path, "json.lua")))()
 
 local LrHttp = import 'LrHttp'
 local LrErrors = import 'LrErrors'
 
-Utils = {}
+--------------------------------------------------------------------------------
+-- Suppress JSON parsing errors
+JSON = (loadfile (LrPathUtils.child(_PLUGIN.path, "json.lua")))()
+local function assert(result, message)
+    logger:error("Failure parsing JSON: " .. message)
+end
 
+JSON.assert = assert
+--------------------------------------------------------------------------------
+
+Utils = {}
+--------------------------------------------------------------------------------
 function Utils.logTable(table)
     for k,v in pairs(table) do
         if type( v ) == 'table' then
@@ -50,23 +59,28 @@ end
 
 function Utils.getJSON( postUrl )
 
-    json = Utils.networkComms( "post", postUrl )
+    data = Utils.networkComms( "post", postUrl )
 
-    if json == nil then
-        logger:info(postUrl .. " was supposed to return JSON, but didn't.")
-        LrErrors.throwUserError("Server problem! \n Response from server was empty, but we were expecting a JSON response!")
+    if data.status and data.status == "error" then
+        logger:error(postUrl .. " was supposed to return JSON, but didn't.")
+        -- Pass the error up the chain
+        return data
     end
 
     -- Now that we have valid JSON, decode it, and try to get the status of our request
     -- If the status is error, show the error to the user, and die.
-    local decode = JSON:decode(json)
+    local decode = JSON:decode(data)
     if decode.status and decode.status == "error" then
-        logger:info("JSON error from Sta.sh")
-        logger:info(json)
-        LrErrors.throwUserError("Error with a JSON response! \n" .. decode.error .. "\n" ..decode.error_description)
+        logger:error("JSON error from " .. postUrl)
+        logger:info(data)
+        return decode
+    elseif decode.status and decode.status == "success" then
+        logger:info("getJSON for " .. postUrl)
+        Utils.logTable(decode)
+        return decode
+    else
+        return {status = "error", from = "json"}
     end
-    Utils.logTable(decode)
-    return decode
 
 
 end
@@ -76,20 +90,15 @@ end
 function Utils.networkComms( action, url )
 
     logger:info("Called Utils.networkComms for " .. url)
-
+    local payload, headers = nil
     -- Do the request
     if action == "post" then
-        local payload, headers = LrHttp.post(url, "")
+        payload, headers = LrHttp.post(url, "")
     else
-        local payload, headers = LrHttp.get(url)
+        payload, headers = LrHttp.get(url)
     end
 
-    local data = Utils.checkResponse( payload, headers, url )
-    if data ~= nil then
-        return data
-    else
-        return nil
-    end
+    return Utils.checkResponse( payload, headers, url )
 
 end
 --------------------------------------------------------------------------------
@@ -98,24 +107,24 @@ function Utils.checkResponse( data, headers, url )
 
     -- If headers.error is set, that means Lightroom had an error.
     if headers and headers.error then
-        logger:info("Lightroom network error:")
+        logger:error("checkResponse: Lightroom network error for url: " .. url)
         Utils.logTable(headers)
-        LrErrors.throwUserError( "Network error: " .. headers.error.errorCode .. "\n" .. headers.error.name )
+        return { status = "error", from = "lightroom", code = headers.error.errorCode, description = headers.error.name }
     end
 
     -- Alternatively, the server could throw back an error.
     -- Only return data if we're sure
     if headers and tonumber(headers.status) > 299 then
-        logger:info("Server error:")
+        logger:error("checkResponse: Server error:" .. headers.status .. "for url: " .. url)
         Utils.logTable(headers)
         logger:info(data)
-        LrErrors.throwUserError( "Remote server returned error code " .. headers.status)
+        return { status = "error", from = "server", code = headers.status, payload = data }
     else
         if data ~= nil then
             return data
         else
-            logger:info("Response for " .. url .. " was empty.")
-            return nil
+            logger:info("checkResponse: Response for " .. url .. " was empty.")
+            return { status = "error", from = "server", code = "empty", payload = data }
         end
     end
 end
