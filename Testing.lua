6073c4a554b92e4fc440164df43013b7b69e0062
Kyle Lexmond <github@kyl191.net>
2012-03-10 16:08:17 +0800

    Reorganise - apparently, can't have a context *within* an async task.

--- Testing.lua
+++ Testing.lua
@@ -4,16 +4,20 @@ local logger = import 'LrLogger'( 'Stash' )
 logger:enable("logfile")
 
 --Utils.md5Files(_PLUGIN.path)
-import "LrTasks".startAsyncTaskWithoutErrorHandler(
-    import "LrFunctionContext".callWithContext( 'Getting remote file', function(context)
-        if context ~= nil then
-            logger:info("Testing: Have a context!")
-        end
-        context:addFailureHandler(function(result,message)
-            logger:error("Testing: Error message: " .. message)
-        end)
-        Utils.logTable(Utils.getJSON("http://kyl191.net/"))
-        end
-    )
+import "LrFunctionContext".callWithContext( 'Getting remote file', function(context)
+    if context ~= nil then
+        logger:info("Testing: Have a context!")
+    end
 
-    )
+    context:addFailureHandler(function(result,message)
+        logger:error("Testing: Error message: " .. message)
+    end)
+
+    import "LrTasks".startAsyncTaskWithoutErrorHandler( function()
+        Utils.logTable(Utils.getJSON("http://kyl191.net/"))
+    end)
+
+end)
+
+
+
