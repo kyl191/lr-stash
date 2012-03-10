c53bca8788b2b5d4c41f969e7daad84765be08db
Kyle Lexmond <github@kyl191.net>
2012-03-10 16:02:25 +0800

    Add a failure cleanup. Better than mucking around with the JSON parser.

--- Testing.lua
+++ Testing.lua
@@ -4,6 +4,16 @@ local logger = import 'LrLogger'( 'Stash' )
 logger:enable("logfile")
 
 --Utils.md5Files(_PLUGIN.path)
-import "LrTasks".startAsyncTask( function()
-    Utils.logTable(Utils.getJSON("http://kyl191.net/"))
-    end)
+import "LrTasks".startAsyncTaskWithoutErrorHandler(
+    import "LrFunctionContext".callWithContext( 'Getting remote file', function(context)
+        if context ~= nil then
+            logger:info("Testing: Have a context!")
+        end
+        context:addFailureHandler(function(result,message)
+            logger:error("Testing: Error message: " .. message)
+        end)
+        Utils.logTable(Utils.getJSON("http://kyl191.net/"))
+        end
+    )
+
+    )
