980556147de66f876943b7372aaae0def29b924d
Kyle Lexmond <github@kyl191.net>
2012-03-10 16:17:31 +0800

    Removed the logging message about having a context, added a cleanup handler that's supposed to be run, and generalised the assert.

--- Testing.lua
+++ Testing.lua
@@ -5,16 +5,20 @@ logger:enable("logfile")
 
 --Utils.md5Files(_PLUGIN.path)
 import "LrFunctionContext".callWithContext( 'Getting remote file', function(context)
-    if context ~= nil then
-        logger:info("Testing: Have a context!")
-    end
 
     context:addFailureHandler(function(result,message)
         logger:error("Testing: Error message: " .. message)
     end)
 
+    context:addCleanupHandler(function(result,message)
+        logger:error("Testing: Cleanup: " .. message)
+    end)
+
     import "LrTasks".startAsyncTaskWithoutErrorHandler( function()
-        Utils.logTable(Utils.getJSON("http://kyl191.net/"))
+        --Utils.logTable(Utils.getJSON("http://kyl191.net/"))
+        logger:info("Going to call the assert!")
+        assert(false, "For some reason I never see this.")
+        logger:info("Called the assert.")
     end)
 
 end)
