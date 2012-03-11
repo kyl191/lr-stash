96edac827fa2af0e48c1a3cf79a962ef1164d928
Kyle Lexmond <github@kyl191.net>
2012-03-12 01:05:16 +0800

    Working auto-update script?

--- Testing.lua
+++ Testing.lua
@@ -3,9 +3,11 @@
 local logger = import 'LrLogger'( 'Stash' )
 logger:enable("logfile")
 local LrPathUtils = import 'LrPathUtils'
+local LrFileUtils = import 'LrFileUtils'
 JSON = (loadfile (LrPathUtils.child(_PLUGIN.path, "json.lua")))()
 
 local Info = require 'Info'
+
 
 --
 import "LrFunctionContext".postAsyncTaskWithContext( 'Getting remote file', function(context)
@@ -18,13 +20,33 @@ import "LrFunctionContext".postAsyncTaskWithContext( 'Getting remote file', func
         logger:error("Testing: Error message: " .. message)
     end)
 
-    local json = JSON:encode(Utils.md5Files(_PLUGIN.path))
-    --Utils.logTable(Info.VERSION, "Plugin version")
     local version = JSON:encode(Info.VERSION)
 
-    --Utils.logTable(Utils.networkComms("post", "http://postbin.heroku.com/67def530?md5=" .. json .. "&version=" .. version), "POSTbin response")
+    local remoteFiles = Utils.getJSON("http://code.kyl191.net/update.php?plugin=" .. _PLUGIN.id)
+    local localFiles = Utils.md5Files(_PLUGIN.path)
 
-    Utils.getJSON("http://kyl191.net")
+    Utils.logTable(remoteFiles, "Remote md5sums")
+    Utils.logTable(localFiles, "Local md5sums")
+    for k, v in pairs (remoteFiles) do
+        if localFiles[k] == v then
+            logger:info("File " .. k .. " is up to date.")
+        else
+            local file = Utils.getFile("http://code.kyl191.net/" .. _PLUGIN.id .. "/head/" .. k, "tempupdatefile", nil)
+            local path = LrPathUtils.makeAbsolute(k, _PLUGIN.path)
+            LrFileUtils.makeFileWritable(path)
+            if LrFileUtils.exists(path) then
+                logger:info("Going to move " .. file .. "to " .. path)
+                Utils.makeBackups(k, 0)
+            end
+            local ok, message = LrFileUtils.move(file, path)
+            if not ok then
+                logger:error(message)
+            end
+
+        end
+
+
+    end
 
 end)
 
