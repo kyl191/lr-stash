b047e4655e5eca0a4b062a5b544ed6f9185d8908
Kyle Lexmond <github@kyl191.net>
2015-08-02 15:17:55 -0700

    Skip chars that are internet acceptable

--- Utils.lua
+++ Utils.lua
@@ -365,7 +365,7 @@ end
 function Utils.urlEncode(str)
     if (str) then
         str = string.gsub (str, "\n", "\r\n")
-        str = string.gsub (str, "([^%w ])",
+        str = string.gsub (str, "([^%w -_.+!*'(),])",
             function (c) return string.format ("%%%02X", string.byte(c)) end)
         str = string.gsub (str, " ", "+")
         end
