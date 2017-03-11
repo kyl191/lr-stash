ad187f8120527d68edf4afd63ffb83ed3b2debb9
Kyle Lexmond <github@kyl191.net>
2017-03-11 15:17:02 -0500

    Remove database insert stuff, entirely on dynamo db now

--- update.php
+++ update.php
@@ -1,5 +1,4 @@
 <?php
-include("db.php");
 require 'vendor/autoload.php';
 
 date_default_timezone_set('UTC');
@@ -50,54 +49,10 @@ if (isset($_GET['plugin']) && (strncasecmp($_GET['plugin'], "net.kyl191.lightroo
 	print_r($_GET);
 }
 
-if(isset($_GET['data']) && $db ){
+if(isset($_GET['data'])){
 	try{
 		$data = @json_decode(@urldecode($_GET['data']), true);
-		// Because we're using hash as an index, check that it's exactly 32 characters long.
-		$hash = $data['hash'];
-		$no_hash = false;
-		if (empty($hash)){
-			$hash = md5($_SERVER['HTTP_X_FORWARDED_FOR']);
-			$no_hash = true;
-		}
-		assert_options(ASSERT_BAIL, true);
-		assert(strlen($hash) == 32);
-		$pluginVersion = $data['pluginVersion']['major'] . "." . $data['pluginVersion']['minor'] . "." . $data['pluginVersion']['revision'];
-		$lightroomVersion = $data['lightroomVersion']['major'] . "." . $data['lightroomVersion']['minor'] . "." . $data['lightroomVersion']['build'] . "." . $data['lightroomVersion']['revision'];
-		$arch = $data['arch'];
-		$os = $data['os'];
-
-		// If the user doesn't want to submit personal data, put placeholders in.
-		if (array_key_exists('username', $data)){
-			$username = $data['username'];
-		} else {
-			$username = "Nil";
-		}
-		if (array_key_exists('uploadCount', $data)){
-			$uploadCount = $data['uploadCount'];
-		} else {
-			$uploadCount = 0;
-		}
-		if (array_key_exists('userSymbol', $data)){
-			$userSymbol = $data['userSymbol'];
-		} else {
-			$userSymbol = "-";
-		}
-
-
-		$vars = array(':hash' => $hash, ':no_hash' => $no_hash, ':arch' => $arch, ':os' => $os, ':lightroomVersion' => $lightroomVersion, ':pluginVersion' => $pluginVersion, ':uploadCount' => $uploadCount, ':userSymbol' => $userSymbol, ':username' => $username);
-
-    $sql = $db->prepare("SELECT id from users WHERE hash = :hash");
-		$sql->execute(array(':hash' => $hash));
-        if ($sql->rowCount() > 0) {
-			$sql = $db->prepare("UPDATE `lrplugin`.`users` SET arch = :arch, lightroomVersion = :lightroomVersion, pluginVersion = :pluginVersion,  os = :os, uploadCount = :uploadCount, userSymbol = :userSymbol, username = :username, no_hash = :no_hash WHERE hash = :hash");
-		} else {
-			$sql = $db->prepare("INSERT INTO `lrplugin`.`users` (`hash`, `arch`, `lightroomVersion`, `pluginVersion`,  `os`, `uploadCount`, `userSymbol`, `username`, `no_hash`) VALUES (:hash, :arch, :lightroomVersion, :pluginVersion, :os, :uploadCount, :userSymbol, :username, :no_hash)");
-		}
-		$sql->execute($vars);
-		$sql->closeCursor();
-
-    $dynamodb = new DynamoDbClient([
+		$dynamodb = new DynamoDbClient([
       'region'   => 'us-west-2',
       'version'  => 'latest',
     ]);
